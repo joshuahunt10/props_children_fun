88c1306cc237179a357f37959faa7fabea39cf45
joshuahunt10 <joshua.hunt88@gmail.com>
2017-07-24 15:03:21 -0500

    light styling

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import '../styles/BaseLayout.css'
+
+import Header from './Header'
+import Footer from './Footer'
 
 export default class BaseLayout extends Component {
   render() {
     return (
-      <div>This should house Header and Footer components and be able to house any children components.</div>
-    );
+      <div>
+        <div className="bars" >
+          <Header/>
+      </div>
+      <div className="body-wrapper">
+        {this.props.children}
+      </div>
+      <div className="bars">
+        <Footer/>
+      </div>
+
+    </div>
+  )
   }
 }
